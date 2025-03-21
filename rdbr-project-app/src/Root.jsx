f748349a351486bfc81adc861ce181bfb4a50c2c
gaboCHLAi <chlaidzegabriel@gmail.com>
2025-03-22 00:21:35 +0400

    Updated Router and config for deployment

--- rdbr-project-app/src/Root.jsx
+++ rdbr-project-app/src/Root.jsx
@@ -5,17 +5,16 @@ import HomePage from "./components/pages/Home/HomePage";
 import AddTask from "./components/pages/AddTask/AddTask";
 import { ActiveProvider } from "./components/Header/Header";
 import TaskInfo from "./components/pages/TaskInfo/TaskInfo";
-
 function Root() {
   return (
     <div>
       <DataProvider>
         <ActiveProvider>
-          <Router basename="/RDBR_PROJECT"> {/* აქაა basename */}
+          <Router basename="/RDBR_PROJECT">
             <Routes>
-              <Route path="/RDBR_PROJECT/" element={<HomePage />} />
-              <Route path="/RDBR_PROJECT/AddTask" element={<AddTask />} />
-              <Route path="/RDBR_PROJECT/task/:taskId" element={<TaskInfo />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="/AddTask" element={<AddTask />} />
+              <Route path="/task/:taskId" element={<TaskInfo />} /> 
             </Routes>
           </Router>
         </ActiveProvider>
