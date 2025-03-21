664026c3d7f8074f76a9906af1e43a860ae95ed6
gaboCHLAi <chlaidzegabriel@gmail.com>
2025-03-22 00:08:51 +0400

    Updated vite.config.js for GitHub Pages

--- rdbr-project-app/vite.config.js
+++ rdbr-project-app/vite.config.js
@@ -1,5 +1,7 @@
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [svgr()],
-};
+export default defineConfig({
+  plugins: [react()],
+  base: "/RDBR_PROJECT/",   
+});
