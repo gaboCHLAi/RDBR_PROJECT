d17127f0349e80b76ab645e667e4480f7afd64ef
gaboCHLAi <chlaidzegabriel@gmail.com>
2025-03-22 00:13:36 +0400

    Updated paths for GitHub Pages

--- rdbr-project-app/src/Root.jsx
+++ rdbr-project-app/src/Root.jsx
@@ -5,6 +5,7 @@ import HomePage from "./components/pages/Home/HomePage";
 import AddTask from "./components/pages/AddTask/AddTask";
 import { ActiveProvider } from "./components/Header/Header";
 import TaskInfo from "./components/pages/TaskInfo/TaskInfo";
+
 function Root() {
   return (
     <div>
@@ -12,9 +13,9 @@ function Root() {
         <ActiveProvider>
           <Router basename="/RDBR_PROJECT">
             <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/AddTask" element={<AddTask />} />
-              <Route path="/task/:taskId" element={<TaskInfo />} /> 
+              <Route path="/RDBR_PROJECT/" element={<HomePage />} />
+              <Route path="/RDBR_PROJECT/AddTask" element={<AddTask />} />
+              <Route path="/RDBR_PROJECT/task/:taskId" element={<TaskInfo />} /> 
             </Routes>
           </Router>
         </ActiveProvider>
